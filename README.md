82cebe6c11580c53fbda44c7fa85025cd8b6728d
WhyAsh5114 <whyash5114@gmail.com>
2022-07-27 23:01:43 +0530

    nextjs initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# token-swapper-algorand
+A platform for startups to acquire funds using Escrow generation
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Title } from '@mantine/core'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Title className="text-white underline w-full text-center">
+        Project Title
+      </Title>
+    </>
+  )
+}
+
+export default Home
